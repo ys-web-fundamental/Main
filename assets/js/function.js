9ba4e9d6b9b37337262b84564d5b92c5c3cbbb3e
yogartgitco <136905024+yogartgitco@users.noreply.github.com>
2025-08-26 07:08:10 +0530

    function
    function

---
+++ assets/js/function.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/*
+This function will take care the addition of two number
+parameter NUmber1 and number 2
+paramter should be integer
+*/
+function addition(number1, number2){
+    var result = number1 + number2;
+    return result;
+}
+
+function subtrction(number1,number2){
+    let sub = number1 - number2;
+    return sub;
+}
+
+function multiplication(num1, num2){
+    let multiplication = num1 * num2;
+    return multiplication;
+}
+
+var data = addition(67,45);
+console.log(data);
+// var data = addition(70,45);
+// console.log(data);
+// var data = addition(80,98);
+// console.log(data);
+// var data = addition(85,15);
+// console.log(data);
+// var data = addition(87,90);
+// console.log(data);
+// var data = addition(83,4444444);
+// console.log(data);
+// var data = addition(2222222222,8888888888);
+// console.log(data);
+// var data = addition(11111,6666666);
+// console.log(data);
+
+
+console.log('------------------------Subtrction------------');
+
+var subtrction = subtrction(78,50);
+console.log(subtrction);
+console.log('------------------------Subtrction------------');
+
+var mul = multiplication(78,50);
+console.log(mul);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 "use strict";
+// Strict Mode class on 25/08/2025
+// var sanket = 'I have money';
+// var spent = false;
+// if(spent) {
+//     sanket = 'I dont have money';
+// }
 
-var sanket = 'I have money';
-var spent = false;
-if(spent) {
-    sanket = 'I dont have money';
+// console.log(sanket);
+
+function fruitprocessor(apples, oranges){
+
+   // console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
 }
 
-console.log(sanket);
+var juice = fruitprocessor(10,50);
+//console.log(juice);
+var juiceapple = fruitprocessor(10,0);
+//console.log(juiceapple);
