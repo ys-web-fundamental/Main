afeda37a07830cd336eb2c58c943bf71432c01e9
vikasbhagat123 <vikasbhagatvb6946037@gmail.com>
2025-08-30 20:03:41 +0530

    New file created for object and array
    Practice for object and array

---
+++ assets/js/objectArrayPractice.js
@@ -0,0 +1,21 @@
+"use strict";
+// Strict Mode class on 25/08/2025
+// var sanket = 'I have money';
+// var spent = false;
+// if(spent) {
+//     sanket = 'I dont have money';
+// }
+
+// console.log(sanket);
+
+function fruitprocessor(apples, oranges){
+
+   // console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+var juice = fruitprocessor(10,50);
+//console.log(juice);
+var juiceapple = fruitprocessor(10,0);
+//console.log(juiceapple);
